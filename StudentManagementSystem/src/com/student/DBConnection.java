80d7fb445bf3dc6082539b89303a2790358764fa
Neel kalshetty <154673531+Neelk1047@users.noreply.github.com>
2026-04-17 23:17:22 +0530

    Change database user constant to placeholder
    Updated the USER constant to indicate a placeholder for the MySQL password.

--- StudentManagementSystem/src/com/student/DBConnection.java
+++ StudentManagementSystem/src/com/student/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     // Database credentials
     private static final String URL = "jdbc:mysql://localhost:3306/studentdb?useSSL=false&allowPublicKeyRetrieval=true";
-    private static final String USER = "root";
+    private static final String USER = "YOUR_PASSWORD";
     
     // IMPORTANT: Replace "password" with your actual MySQL password!
     private static final String PASSWORD = "root"; 
